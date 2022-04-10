eebf8d00c76f79d71a51b737fdce93ae988dd9f0
Wojciech Jelen <w.jelen20@gmail.com>
2022-04-11 01:01:18 +0200

    feature: saves addresses to local storage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
-import { useEffect } from "react";
 import { useQueries } from "react-query";
 
 import { getTokensBalances } from "../api";
+
+const localStorageKey = "tokensBalances";
 
 const RANDOM_ADDRESS = "0xd54921ccf100cc6a78acbbd4e55fd57c9b50ea7a";
 const ADDRESSES = [
@@ -13,17 +15,49 @@ const ADDRESSES = [
 ];
 
 const Home: NextPage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [trackedAddresses, setTrackedAddresses] = useState<string[]>([]);
+
+  useEffect(() => {
+    const storedAddresses = localStorage.getItem(localStorageKey);
+    if (storedAddresses) {
+      setTrackedAddresses(JSON.parse(storedAddresses));
+    }
+  }, []);
+
   const results = useQueries(
     ADDRESSES.map((address) => {
       return {
         queryKey: ["balances", address],
         queryFn: () => getTokensBalances(address),
-        refetchInterval: 5000,
+        // refetchInterval: 5000,
       };
     })
   );
 
-  console.log("###results", results);
+  const onInputChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      event.preventDefault();
+      setInputValue(event.target.value);
+    },
+    []
+  );
+
+  const onTrackAddress = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      const newAdressesList = [...trackedAddresses, inputValue];
+      setTrackedAddresses(newAdressesList);
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(
+          localStorageKey,
+          JSON.stringify(newAdressesList)
+        );
+      }
+      setInputValue("");
+    },
+    [trackedAddresses, inputValue]
+  );
 
   return (
     <div>
@@ -33,7 +67,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <label htmlFor="address">address</label>
+        <input
+          name="address"
+          id="address"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button onClick={onTrackAddress}>Track address</button>
+        <div>{JSON.stringify(trackedAddresses, null, 2)}</div>
+      </main>
     </div>
   );
 };
