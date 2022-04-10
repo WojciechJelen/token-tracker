b9e972e47b4c26c54dc4f041cc6c629b740f7abd
Wojciech Jelen <w.jelen20@gmail.com>
2022-04-10 23:08:59 +0200

    init:project celanup

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const ALCHEMY_URL =
+  "https://eth-mainnet.alchemyapi.io/v2/sgh9NP9FUFD8l1-fa5ox534Vm8qMDRc9";
+
+export const LINK = "0x514910771af9ca656af840dff83e8264ecf986ca";
+export const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+export const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
