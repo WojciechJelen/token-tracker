7452b51a58293aaa842df596184274047283ec7d
Wojciech Jelen <w.jelen20@gmail.com>
2022-04-12 00:36:25 +0200

    formats units

---
+++ emotion.d.ts
@@ -0,0 +1,13 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    primary: string;
+    accentSecondary: string;
+    accent: string;
+    text: string;
+    background: string;
+    tableHeaderBackground: string;
+    tableHeaderText: string;
+  }
+}
--- utils/index.ts
+++ utils/index.ts
@@ -7,3 +7,10 @@ const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_URL);
 export const contractFactory = (address: string) => {
   return new ethers.Contract(address, minimalERC20ABI, provider);
 };
+
+export const formatUnit = (amount: ethers.BigNumberish) => {
+  if (!amount) {
+    return "0";
+  }
+  return ethers.utils.formatUnits(amount, 6);
+};
