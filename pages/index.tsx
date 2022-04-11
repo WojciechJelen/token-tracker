af4992eea15c434ebd47ff4962b3bdb6c4498ef5
Wojciech Jelen <w.jelen20@gmail.com>
2022-04-11 09:42:45 +0200

    feat(table): displays tracked adresses raw

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Head from "next/head";
 import { useQueries } from "react-query";
+import { useTable } from "react-table";
 
 import { getTokensBalances } from "../api";
+import { ethers } from "ethers";
 
 const localStorageKey = "tokensBalances";
 
-const RANDOM_ADDRESS = "0xd54921ccf100cc6a78acbbd4e55fd57c9b50ea7a";
 const ADDRESSES = [
-  RANDOM_ADDRESS,
+  "0xd54921ccf100cc6a78acbbd4e55fd57c9b50ea7a",
   "0x6b175474e89094c44da98b954eedeac495271d0f",
   "0x20116b72c7f5ed183060357ac65219bae8f4932b",
 ];
@@ -26,7 +27,7 @@ const Home: NextPage = () => {
   }, []);
 
   const results = useQueries(
-    ADDRESSES.map((address) => {
+    trackedAddresses.map((address) => {
       return {
         queryKey: ["balances", address],
         queryFn: () => getTokensBalances(address),
@@ -59,6 +60,72 @@ const Home: NextPage = () => {
     [trackedAddresses, inputValue]
   );
 
+  console.log("###results", results);
+
+  const isLoading =
+    results.some((resultResponse) => {
+      resultResponse.isFetching || resultResponse.isRefetching;
+    }) || false;
+
+  const hasError =
+    results.some((resultResponse) => {
+      resultResponse.error ||
+        resultResponse.isError ||
+        resultResponse.isRefetchError ||
+        resultResponse.isLoadingError;
+    }) || false;
+
+  const data = useMemo(() => {
+    const data = results.map((resultResponse) => {
+      const { data } = resultResponse;
+      console.log("###DATA", data);
+      return {
+        address: data?.address,
+        daiBalance: data?.daiBalance?.toString() ?? "",
+        usdtBalance: data?.usdtBalance?.toString() ?? "",
+        linkBalance: data?.linkBalance?.toString() ?? "",
+      };
+    });
+
+    return data;
+  }, [results]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Address",
+        accessor: "address", // accessor is the "key" in the data
+      },
+      {
+        Header: "DAI Balance",
+        accessor: "daiBalance",
+      },
+      {
+        Header: "USDT Balance",
+        accessor: "usdtBalance",
+      },
+      {
+        Header: "Link Balance",
+        accessor: "linkBalance",
+      },
+    ],
+    []
+  );
+
+  /* @ts-ignore */
+  const tableInstance = useTable({ columns, data });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (hasError) {
+    return <div>Something went wrong...</div>;
+  }
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
   return (
     <div>
       <Head>
@@ -76,7 +143,43 @@ const Home: NextPage = () => {
           onChange={onInputChange}
         />
         <button onClick={onTrackAddress}>Track address</button>
-        <div>{JSON.stringify(trackedAddresses, null, 2)}</div>
+        <div>
+          {JSON.stringify(trackedAddresses, null, 2)}
+
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()} key={column.id}>
+                      {column.render("Header")}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()} key={row.id}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td
+                          {...cell.getCellProps()}
+                          key={cell.getCellProps().key}
+                        >
+                          {cell.render("Cell")}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   );
