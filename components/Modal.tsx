0d574021a5b957fe57ca7a9404e9bb5b6a7f4040
Wojciech Jelen <w.jelen20@gmail.com>
2022-04-11 18:20:27 +0200

    feat: added modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as ReactDOM from "react-dom";
+import styled from "@emotion/styled";
+
+type PropsType = {
+  show: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+export const Modal: React.FC<PropsType> = ({ show, onClose, children }) => {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  const handleCloseClick = (e: any) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  const modalContent = show ? (
+    <StyledModalOverlay>
+      <StyledModal>
+        <StyledModalHeader>
+          <a href="#" onClick={handleCloseClick}>
+            x
+          </a>
+        </StyledModalHeader>
+        <h3>Add address to track balances</h3>
+        <StyledModalBody>{children}</StyledModalBody>
+      </StyledModal>
+    </StyledModalOverlay>
+  ) : null;
+
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.querySelector("#modal-root")!
+    );
+  } else {
+    return null;
+  }
+};
+
+const StyledModalBody = styled.div`
+  padding-top: 10px;
+`;
+
+const StyledModalHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  font-size: 25px;
+`;
+
+const StyledModal = styled.div`
+  background: white;
+  width: 500px;
+  height: 600px;
+  border-radius: 15px;
+  padding: 15px;
+`;
+const StyledModalOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export default Modal;
